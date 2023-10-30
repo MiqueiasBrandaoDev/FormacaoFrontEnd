6f1a3176ef7ba1c78b8eb02799aa737d6dd801f4
MiqueiasBrandaoDev <miqueiasbrandaogyn@gmail.com>
2023-10-30 17:32:03 -0300

    updade curso js

---
+++ Curso JavaScript/js/estrutura_de_programacao.js
@@ -0,0 +1,12 @@
+// método Math - método para números
+// max() - retorna o maior numero
+console.log(Math.max(2,3,55,43,232))
+
+//min() - retorna o menor numero
+console.log(Math.min(2,3,55,43,232))
+
+//flor() - retorna o numero arredondado para baixo
+console.log(Math.floor(2.3432))
+
+//ceil() - retorna o numero arredondado para cima
+console.log(Math.ceil(2.3432))
