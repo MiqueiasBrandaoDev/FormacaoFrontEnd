7072a4a1c3d5d43873d5dca37d05820398751550
MiqueiasBrandaoDev <miqueiasbrandaogyn@gmail.com>
2023-10-30 17:31:12 -0300

    update curso

--- Curso JavaScript/js/script.js
+++ Curso JavaScript/js/script.js
@@ -1,11 +1,15 @@
 document.addEventListener("click", function(click){
-    
+
     if(click.pointerId == 1){
-
         var element = document.querySelector("main");
-
-        element.classList.toggle("hide");  
-
+        //element.classList.toggle("hide");  
     }
 
 })
+
+
+//obtendo arquivo a selecionar - ativar somente o script que foi selecionado pelo usuário
+let arquive = document.querySelector("#cars").value
+let lineScript = document.querySelectorAll(`script #estrutura_de_programacao`)
+console.log(arquive)
+console.log(lineScript)
