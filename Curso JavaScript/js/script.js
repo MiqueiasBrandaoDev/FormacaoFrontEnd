44770d08153029d5d72afee7d6ab1ccf36f39334
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-10-30 21:02:35 -0300

    update - sistema de aprendizado

--- Curso JavaScript/js/script.js
+++ Curso JavaScript/js/script.js
@@ -8,8 +8,57 @@ document.addEventListener("click", function(click){
 })
 
 
-//obtendo arquivo a selecionar - ativar somente o script que foi selecionado pelo usuário
-let arquive = document.querySelector("#cars").value
-let lineScript = document.querySelectorAll(`script #estrutura_de_programacao`)
-console.log(arquive)
-console.log(lineScript)
+document.querySelector("#module").addEventListener("change", function(event){
+    let selecinado = document.querySelector("#module").value
+
+    // Salva o valor no localStorage
+    localStorage.setItem("opcaoSelecionada", selecinado);
+
+    //exclui o elemento anterior
+    let existElement = document.querySelector(".dynamic-script")
+    if(existElement){
+        existElement.classList.remove("dynamic-script")
+        existElement.remove()
+    }
+
+    //insere o novo elemento
+    let newScript = document.createElement('script')
+    newScript.setAttribute('src', `js/${selecinado}.js`)
+    document.querySelector("body").appendChild(newScript)   
+
+    //limpar console.log
+    console.clear();
+})
+
+
+// Verifica se há uma opção salva no localStorage ao carregar a página
+window.onload = function() {
+    // Obtém a opção salva no localStorage
+    var opcaoSalva = localStorage.getItem("opcaoSelecionada");
+    
+    // Se houver uma opção salva, define-a como a opção selecionada no select
+    if (opcaoSalva) {
+        document.getElementById("module").value = opcaoSalva;
+
+        let selecinado = document.querySelector("#module").value
+
+        // Salva o valor no localStorage
+        localStorage.setItem("opcaoSelecionada", selecinado);
+
+        //exclui o elemento anterior
+        let existElement = document.querySelector(".dynamic-script")
+        if(existElement){
+            existElement.classList.remove("dynamic-script")
+            existElement.remove()
+        }
+
+        //insere o novo elemento
+        let newScript = document.createElement('script')
+        newScript.setAttribute('src', `js/${selecinado}.js`)
+        document.querySelector("body").appendChild(newScript)   
+
+        //limpar console.log
+        console.clear();
+
+    }
+  };
