e680d590debfa24ada695d726081edf06f397313
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-11-02 17:56:41 -0300

    update deputação

--- Curso JavaScript/2.JavaScript Intermediário/1.Orientação a Objetos/js/script.js
+++ Curso JavaScript/2.JavaScript Intermediário/1.Orientação a Objetos/js/script.js
@@ -53,6 +53,11 @@ class Paciente{
 }
 
 //instanciando a class com os valores
+
+//definindo valor fixo da propriedade para um elemento no prototype
+const dependentes = Symbol()
+Paciente.prototype[dependentes] = 2
+
 let newName = new Paciente("Maria", 123, "Unimed")
 newName.getPacient()
 console.log("Objeto Antes da Alteração:")
@@ -65,3 +70,84 @@ newName.updatePacientCodigo(87123976)//alterando dado  [codigo] da class
 //exibindo o objeto atual com as alterações
 console.log("Objeto abaixo alterado:")
 console.log(newName.getPacient())
+
+//obter valor fixo configurado prototype
+console.log(newName[dependentes])
+console.clear()
+
+//simulando um comentário de alguma página
+class Coment{
+
+    //construindo os atributos
+    constructor(title, id, username, tags){
+        this.title = title
+        this.id = id
+        this.username = username
+        this.tags = tags
+    }
+
+    //usando o GET para exibir/retornar os dados
+    get getComent(){
+        return {
+            "title": this.title,
+            "id": this.id,
+            "username": this.username,
+            "tags": this.tags
+        }
+    }
+
+    //usando o SET para alterar/Setar algum atributo
+    set setComentTags(tags){
+        const tagsArray = tags.split(", ")
+        this.tags = tagsArray
+    }
+
+    //método para alterar um atributo usando SET
+    set updateTitle(newtitle){
+        this.title = newtitle
+    }
+}
+
+//instanciando a classe e inserindo os atributos
+const newComent = new Coment("title teste", 123, "miqueiasbrandaoo")
+
+//setando as tags com o atributo SET usado na construção do método
+newComent.setComentTags = "desenvolvimento, evolução, constancia, aprendizado"
+
+//exibindo o objeto criado
+console.log(newComent)
+
+//alterando o titulo
+newComent.updateTitle = "Titulo alterado"
+
+//simulando envio do objeto para o banco de dados
+const enviarBD = newComent
+console.log(enviarBD)
+
+//nova aula de herança
+console.clear()
+
+// classe pai
+class Eu{
+    constructor(nome){
+        this.nome = nome
+    }
+}
+
+class Logout extends Eu{
+    constructor(nome){
+
+        //alterando a propriedade da class pai
+        super(nome)
+
+        //constinuar a construção dos atributos
+        this.isLogout = true
+
+    }
+}
+
+const login = new Logout("Miqueias_brandaoo")
+console.log(login)
+
+//retorna se uma instancia faz parte da classe informada
+console.log(login instanceof Logout)
---
+++ Curso JavaScript/2.JavaScript Intermediário/2.Depuração com JavaScript/js/script.js
@@ -0,0 +1,11 @@
+//strict - boa pratica para tornar o codigo seguro
+"use strict"
+let a = 123//não aceita
+
+
+//console.log
+console.log("chegou aqui")
+
+
+//debuger
+debugger//o navegador vai parar neste ponto do codigo
