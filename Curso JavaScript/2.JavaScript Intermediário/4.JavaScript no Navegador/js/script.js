f2b007bfc144440a0491a34f63225bee137d5fb4
MiqueiasBrandaoDev <miqueiasbrandaogyn@gmail.com>
2023-11-03 17:17:47 -0300

    update DOM

--- Curso JavaScript/2.JavaScript Intermediário/4.JavaScript no Navegador/js/script.js
+++ Curso JavaScript/2.JavaScript Intermediário/4.JavaScript no Navegador/js/script.js
@@ -1,5 +1,19 @@
 let newDiv = document.createElement("div")
-newDiv.textContent = "Div Criada via JS/DOM"
+newDiv.textContent = "Div Criada via JS/DOM com insertBefore"
 newDiv.setAttribute("id", "div-com-js")
 newDiv.setAttribute("class", "div-com-js")
-document.querySelector("body").appendChild(newDiv)
+//insertBefore - insere antes de um elemento, setando primeiramento um elemento pai
+document.querySelector("body").insertBefore(newDiv, document.querySelector("h1"))
+
+
+
+let newDivComAppendChild = document.createElement("div")
+newDivComAppendChild.textContent = "Div Criada via JS/DOM"
+newDivComAppendChild.setAttribute("id", "div-com-js")
+newDivComAppendChild.setAttribute("class", "div-com-js")
+//appendChild - insere um elemento dentro de outro elemento
+newDivComAppendChild.textContent = "Agora com o appendChild"
+document.body.appendChild(newDivComAppendChild)
+
+
+document.querySelector("h1").remove()
