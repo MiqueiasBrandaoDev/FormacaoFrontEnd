0a059806b049db6e1a788efcb8ae76abb11c84da
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-11-05 11:23:51 -0300

    update project taboada

--- Curso JavaScript/2.JavaScript Intermediário/4.JavaScript no Navegador/js/script.js
+++ Curso JavaScript/2.JavaScript Intermediário/4.JavaScript no Navegador/js/script.js
@@ -17,3 +17,34 @@ document.body.appendChild(newDivComAppendChild)
 
 
 document.querySelector("h1").remove()
+
+
+let eventDesconto = false
+document.addEventListener("mousemove", (e) => {
+
+
+    if(e.y <= 20 && eventDesconto === false){
+        
+        eventDesconto = true
+        let newElement = document.createElement("h1")
+        newElement.textContent = "Não saia agora, resgate 50% de desconto!"
+        newElement.setAttribute("id", "desconto")
+        document.querySelector("body").appendChild(newElement)
+        
+        let btn = document.createElement("button")
+        btn.textContent = "Resgate aqui seu desconto"
+        btn.setAttribute("id", "desconto-btn")
+        document.querySelector("body").appendChild(btn)
+
+    }
+
+    if(document.querySelector("#desconto") && e.y > 22){
+
+        document.querySelector("#desconto").remove()
+        document.querySelector("#desconto-btn").remove()
+        eventDesconto = false
+
+    }
+
+})
+
